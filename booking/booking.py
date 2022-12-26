82e5973fdeee800e222f7793ac191825fb8db01f
BabychOleksii <babych.oleksii@gmail.com>
2022-12-26 16:13:07 +0200

    Changing the currency

--- booking/booking.py
+++ booking/booking.py
@@ -1,5 +1,7 @@
 import booking.constants as const
+import time
 import os
+from selenium.webdriver.common.by import By
 from selenium import webdriver
 
 
@@ -9,6 +11,8 @@ class Booking(webdriver.Chrome):
         self.teardown = teardown
         os.environ['PATH'] += self.driver_path
         super(Booking, self).__init__()
+        self.implicitly_wait(15)
+        self.maximize_window()
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         if self.teardown:
@@ -16,3 +20,15 @@ class Booking(webdriver.Chrome):
 
     def land_first_page(self):
         self.get(const.BASE_URL)
+
+    def change_currency(self, currency=None):
+        currency_element = self.find_element(
+            By.CSS_SELECTOR, value='button[data-tooltip-text="Choose your currency"]'
+        )
+        currency_element.click()
+        selected_currency_element = self.find_element(
+            By.CSS_SELECTOR, value=f'a[data-modal-header-async-url-param*="selected_currency={currency}"]'
+        )
+        selected_currency_element.click()
+        # time.sleep(30)
+
